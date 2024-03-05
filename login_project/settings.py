df4b80a88445afa71e509f6d6fcfdeb14ba5ce44
Jovellmiranda <jcqmiranda.student@ua.edu.ph>
2024-03-05 16:10:33 +0800

    Unfinished - Signup

--- login_project/settings.py
+++ login_project/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+    
 ]
 
 MIDDLEWARE = [
@@ -121,5 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
