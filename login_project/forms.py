3cbf0a10a8b553b5ab8d7c097d5d8881e724c6ae
Jovellmiranda <jcqmiranda.student@ua.edu.ph>
2024-03-05 16:45:51 +0800

    Age

--- login_project/forms.py
+++ login_project/forms.py
@@ -6,6 +6,7 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
     email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
+    age = forms.IntegerField(min_value=0,max_value=100, help_text='Enter a valid email address')
     class Meta:
         model=User
-        fields=['username','first_name','last_name','email', 'password1','password2', ]
+        fields=['username','first_name','last_name','email','age', 'password1','password2', ]
