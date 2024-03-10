62788c5541c328fabcb44e2005268277182f5658
Jipee01 <jp22talidong@gmail.com>
2024-03-10 13:19:47 +0800

    Jipe update

--- login_project/forms.py
+++ login_project/forms.py
@@ -3,10 +3,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    username = forms.CharField(min_length=6, required=False, help_text='Optional')
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required')
     email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
-    age = forms.IntegerField(min_value=0,max_value=100, help_text='Enter a valid email address')
+    age = forms.IntegerField(min_value=1, max_value=100, help_text='Enter a valid age')
+
     class Meta:
-        model=User
-        fields=['username','first_name','last_name','email','age', 'password1','password2', ]
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'age', 'password1', 'password2']
