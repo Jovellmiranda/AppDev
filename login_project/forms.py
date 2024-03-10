880fa297f9e019e208fa58e41949636a51fa63b7
Jipee01 <jp22talidong@gmail.com>
2024-03-10 14:14:13 +0800

    new

--- login_project/forms.py
+++ login_project/forms.py
@@ -1,14 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from datetime import datetime
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(min_length=6, required=False, help_text='Optional')
-    first_name = forms.CharField(max_length=30, required=True, help_text='Required')
-    last_name = forms.CharField(max_length=30, required=True, help_text='Required')
-    email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
-    age = forms.IntegerField(min_value=1, max_value=100, help_text='Enter a valid age')
+    Full_Name = forms.CharField(min_length=6, required=True, help_text='Required')
+    Phone_Number = forms.CharField(max_length=30, required=False, help_text='Optional')
+    Email_Address = forms.EmailField(max_length=254, required=False, help_text='Optional')
+    Date_of_Birth = forms.IntegerField(help_text='Enter your date of birth as a number (DD/MM/YYYY)',
+                                       widget=forms.TextInput(attrs={'placeholder': 'DD/MM/YYYY'}))
+
+    Password = forms.CharField(min_length=8, widget=forms.PasswordInput, help_text='Enter a valid password')
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'age', 'password1', 'password2']
+        fields = ['Full_Name', 'Phone_Number', 'Email_Address', 'Date_of_Birth', 'password1', 'password2']
+
+    def clean_Date_of_Birth(self):
+        dob_number = self.cleaned_data.get('Date_of_Birth')
+        if dob_number:
+            try:
+                dob_str = str(dob_number)
+                dob_combined = datetime.strptime(dob_str, '%d%m%Y').date()
+                return dob_combined
+            except ValueError:
+                raise forms.ValidationError("Invalid date format. Please enter DD/MM/YYYY.")
+        return dob_number
